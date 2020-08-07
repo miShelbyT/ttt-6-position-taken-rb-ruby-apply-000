4ef1cd289aa0cc5ba160be04cb3219b759451c75
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-07 20:34:33 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
 # code your #position_taken? method here!
+def position_taken(board, index)
+  if "   "
+    false
+  elsif "  "
+    false
+  elsif " "
+    false
+  else
+    true
+  end
+end
+    
