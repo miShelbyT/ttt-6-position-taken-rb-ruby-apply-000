2ea0a24c8e2e9b29d82260f2196888b57df36767
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-07 21:24:53 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if array[index_number] == "   "
+  if board[index] == "   "
     return false
-  elsif array[index_number] == "  " || " " || nil
+  elsif board[index] == "  " || " " || nil
     return false
     else
     return true
